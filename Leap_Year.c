cbb67ff2ea4e7b2b125918c77876df3bbe21c211
Anvi Bagora <anvibagora@gmail.com>
2026-07-08 10:46:11 +0000

    save

---
+++ Leap_Year.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+void main(){
+    int year;
+    printf("Enter year: ");
+    scanf("%d",&year);
+    if(year%400==0){
+        printf("Leap Year");
+    }else if(year%100==0){
+        printf("Not a leap year");
+    }else if(year%4==0){
+        printf("Leap Year");
+    }else{
+        printf("Not a leap Year");
+    }
+}
